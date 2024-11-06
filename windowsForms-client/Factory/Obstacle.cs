96f2351dc993b9ba88c929338b9f1f62cfe4c39e
Aukse <126973619+Auks3lis@users.noreply.github.com>
2024-11-07 00:05:11 +0200

    Pridejau Factory/Prototype/Strategy

---
+++ windowsForms-client/Factory/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using windowsForms_client.Strategy;
+
+namespace windowsForms_client
+{
+	public class Obstacle
+	{
+		public int x_coordinate { get; set; }
+		public int y_coordinate { get; set; }
+        public IStrategy Strategy { get; set; }
+        public bool HasBeenAffected { get; set; } = false; 
+
+        public Obstacle( int x, int y, IStrategy strategy)
+		{
+			this.x_coordinate = x;
+			this.y_coordinate = y;
+            Strategy = strategy;
+        }
+	}
+
+}
