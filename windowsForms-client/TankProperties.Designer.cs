09e876b1584fe5f89b519b7188ced63afd2ac688
vyteniskri <krisciunas.vytenis@gmail.com>
2024-11-09 12:22:19 +0200

    Nauja .NET 8.0 versija
    Projekto versija buvo 4.2.7, o json 8.0. Del to crashino :d

--- windowsForms-client/TankProperties.Designer.cs
+++ windowsForms-client/TankProperties.Designer.cs
@@ -31,72 +31,69 @@ namespace windowsForms_client
         /// </summary>
         private void InitializeComponent()
         {
-            this.TankType = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.Upgrade = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            TankType = new ComboBox();
+            button1 = new Button();
+            Upgrade = new ComboBox();
+            label1 = new Label();
+            SuspendLayout();
             // 
             // TankType
             // 
-            this.TankType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.TankType.FormattingEnabled = true;
-            this.TankType.Items.AddRange(new object[] {
-            "Pistol",
-            "TommyGun"});
-            this.TankType.Location = new System.Drawing.Point(309, 204);
-            this.TankType.Name = "TankType";
-            this.TankType.Size = new System.Drawing.Size(165, 21);
-            this.TankType.TabIndex = 0;
+            TankType.DropDownStyle = ComboBoxStyle.DropDownList;
+            TankType.FormattingEnabled = true;
+            TankType.Items.AddRange(new object[] { "Pistol", "TommyGun", "Shotgun" });
+            TankType.Location = new Point(360, 235);
+            TankType.Margin = new Padding(4, 3, 4, 3);
+            TankType.Name = "TankType";
+            TankType.Size = new Size(192, 23);
+            TankType.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(355, 250);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Confirm";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Location = new Point(414, 288);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 27);
+            button1.TabIndex = 1;
+            button1.Text = "Confirm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Upgrade
             // 
-            this.Upgrade.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.Upgrade.FormattingEnabled = true;
-            this.Upgrade.Items.AddRange(new object[] {
-            "-",
-            "Movement speed boost",
-            "Shooting speed boost",
-            "Health boost",
-            "Shield boost"});
-            this.Upgrade.Location = new System.Drawing.Point(390, 138);
-            this.Upgrade.Name = "Upgrade";
-            this.Upgrade.Size = new System.Drawing.Size(146, 21);
-            this.Upgrade.TabIndex = 3;
+            Upgrade.DropDownStyle = ComboBoxStyle.DropDownList;
+            Upgrade.FormattingEnabled = true;
+            Upgrade.Items.AddRange(new object[] { "-", "Movement speed boost", "Shooting speed boost", "Health boost", "Shield boost" });
+            Upgrade.Location = new Point(455, 159);
+            Upgrade.Margin = new Padding(4, 3, 4, 3);
+            Upgrade.Name = "Upgrade";
+            Upgrade.Size = new Size(170, 23);
+            Upgrade.TabIndex = 3;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(144, 141);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(240, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Select one upgrade to receive every 10 seconds:";
+            label1.AutoSize = true;
+            label1.Location = new Point(168, 163);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(257, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Select one upgrade to receive every 10 seconds:";
             // 
             // TankProperties
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Upgrade);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.TankType);
-            this.Name = "TankProperties";
-            this.Text = "TankProperties";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(933, 519);
+            Controls.Add(label1);
+            Controls.Add(Upgrade);
+            Controls.Add(button1);
+            Controls.Add(TankType);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "TankProperties";
+            Text = "TankProperties";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
 
