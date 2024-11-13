99819a33549284e1860f7cf75f4c014b36575160
Aukse <126973619+Auks3lis@users.noreply.github.com>
2024-11-13 18:12:13 +0200

    Clone fixed

--- windowsForms-client/Prototype/Coin.cs
+++ windowsForms-client/Prototype/Coin.cs
@@ -8,30 +8,58 @@ namespace windowsForms_client.Prototype
 {
     public class Coin : IPrototype<Coin>
     {
+        private const string pathB = @"c:\pic\";
         public string Type { get; set; }
-        public Position Position { get; set; } //Lacking creativity :D
+        public int X { get; set; }
+        public int Y { get; set; }
+        public CoinDetails Details { get; set; }
 
-        public Coin(string type, int x, int y)
+        public Coin(string type, int x, int y, CoinDetails details)
         {
             Type = type;
-            Position = new Position(x, y);
+            X = x; 
+            Y = y;
+            Details = details;
         }
 
         public Coin ShallowCopy()
         {
-            return (Coin)this.MemberwiseClone();
+            Coin clone = (Coin)this.MemberwiseClone();
+            clone.X = new Random().Next(0, 800);
+            clone.Y = new Random().Next(0, 300);
+            return clone;
         }
 
         public Coin DeepCopy()
         {
             Coin clone = (Coin)this.MemberwiseClone();
-            clone.Position = new Position(Position.X, Position.Y);
+            clone.X = new Random().Next(0, 800);
+            clone.Y = new Random().Next(0, 300);
+            string newType = GetNextCoinType(); 
+            clone.Type = newType;
+            string imagePath = GetImagePathForCoin(newType);
+            clone.Details = new CoinDetails(Details.Value + 1, imagePath, Image.FromFile(imagePath));
+
             return clone;
         }
 
         public override int GetHashCode()
         {
             return base.GetHashCode();
+        }
+        private string GetNextCoinType()
+        {
+
+            if (Type == "Gold") return "Diamond";
+            else if (Type == "Diamond") return "Emerald";
+            else return Type; 
+        }
+        private string GetImagePathForCoin(string coinType)
+        {
+            if (coinType == "Gold") return pathB +  "gold.jpg";
+            else if (coinType == "Diamond") return pathB +  "diamond.jpg";
+            else if (coinType == "Emerald") return pathB +  "emerald.jpg";
+            else return pathB + "gold.jpg";
         }
     }
 }
