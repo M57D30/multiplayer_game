5af758faf566e04ff9f0e682b45a965921eeaf52
Aukse <126973619+Auks3lis@users.noreply.github.com>
2024-11-13 15:52:33 +0200

    Strategy improved

--- windowsForms-client/Factory/Obstacle.cs
+++ windowsForms-client/Factory/Obstacle.cs
@@ -12,14 +12,35 @@ namespace windowsForms_client
 		public int x_coordinate { get; set; }
 		public int y_coordinate { get; set; }
         public IStrategy Strategy { get; set; }
-        public bool HasBeenAffected { get; set; } = false; 
+        public bool HasBeenAffected { get; set; } = false;
 
+        private Color OriginalColor { get; set; }
+        private Color TempColor { get; set; }
         public Obstacle( int x, int y, IStrategy strategy)
 		{
 			this.x_coordinate = x;
 			this.y_coordinate = y;
             Strategy = strategy;
         }
-	}
-
+        public void SetTempColor(Color color)
+        {
+            TempColor = color;
+        }
+        public void SetOriginalColor(Color color)
+        {
+            OriginalColor = color;
+        }
+        public Color GetTempColor()
+        {
+            return this.TempColor;
+        }
+        public Color GetOriginalColor()
+        {
+            return this.OriginalColor;
+        }
+        public void ResetColor()
+        {
+            TempColor = OriginalColor;
+        }
+    }
 }
