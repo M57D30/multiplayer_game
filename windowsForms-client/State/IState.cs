a68cc329a544fbeb6a913a71dfeb115900bd513a
Aukse <126973619+Auks3lis@users.noreply.github.com>
2024-12-19 00:41:43 +0200

    STATE - bet kulkos normaliai neveikia ir displayinu lokalu priesu healthbar
    HELP ME

---
+++ windowsForms-client/State/IState.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windowsForms_client.State
+{
+    public interface IState
+    {
+        public void HandleChange(Tank tank);
+    }
+    public class Healthy : IState
+    {
+        public void HandleChange(Tank tank)
+        {
+            if (tank.Health <= tank.MaxHealth * 0.75)
+            {
+                tank.SetState(new Damaged());
+                Console.WriteLine($"From healthy to damaged");
+            }
+        }
+    }
+
+    public class Damaged : IState
+    {
+        public void HandleChange(Tank tank)
+        {
+            if (tank.Health <= tank.MaxHealth * 0.25)
+            {
+                tank.SetState(new Critical());
+                Console.WriteLine($"From damaged to critical");
+
+            }
+            else if (tank.Health > tank.MaxHealth * 0.75)
+            {
+                tank.SetState(new Healthy());
+                Console.WriteLine($"From damaged to healthy");
+
+            }
+        }
+    }
+    public class Critical : IState
+    {
+        public void HandleChange(Tank tank)
+        {
+            if (tank.Health <= 0)
+            {
+                tank.SetState(new Dead());
+                Console.WriteLine($"From critical to dead");
+
+            }
+            else if (tank.Health > tank.MaxHealth * 0.25)
+            {
+                tank.SetState(new Damaged());
+                Console.WriteLine($"From critical to damaged");
+
+            }
+        }
+    }
+    public class Dead : IState
+    {
+        public void HandleChange(Tank tank)
+        {
+            tank.SetState(this);
+            Console.WriteLine($"{tank.GetType()} is dead");
+        }
+    }
+
+}
